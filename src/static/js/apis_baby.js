caf5230607fc5513af53ddb2873c4a31af9d7358
a2730864582 <2730864582@qq.com>
2020-10-05 20:11:21 +0800

    v1.0.6

--- src/static/js/apis_baby.js
+++ src/static/js/apis_baby.js
@@ -9,5 +9,36 @@ let apis_baby = {
       data
     }).getData()
   },
+  // 活动现场
+  async actPhotoList (data) {
+    return await new Api({
+      url:"/sys/actPhoto/list",
+      method:"POST",
+      data
+    }).getData()
+  },
+  // 书院档案
+  async agreementView (data) {
+    return await new Api({
+      url:"/sys/agreement/web/getCanteenAgreementView",
+      method:"POST",
+      data
+    }).getData()
+  },
+  // 公告列表
+  async noticeList (data) {
+    return await new Api({
+      url:"/system/notice/web/list",
+      method:"POST",
+      data
+    }).getData()
+  },
+  // 公告详情
+  async noticeSel (noticeId) {
+    return await new Api({
+      url:`/system/notice/web/sel/${noticeId}`,
+      method:"GET"
+    }).getData()
+  },
 };
 module.exports = apis_baby;
